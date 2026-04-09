7ba897388e38ef4bce96d1d91692edb948a1f4da
mrauthentik <umokeuchenna2020@gmail.com>
2026-04-09 12:16:31 +0100

    fix: setup user form , for users input

--- user-management-app/src/components/UserForm.tsx
+++ user-management-app/src/components/UserForm.tsx
@@ -1,48 +1,49 @@
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { userSchema, type UserFormData } from '../schemas/userSchema';
-import { createUser } from '../services/userService';
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { userSchema, type UserFormData } from '../schemas/userSchema'
+import { createUser } from '../services/userService'
 
 interface UserFormProps {
-    onSuccess: () => void
+  onSuccess: () => void
 }
 
-const UserForm = ({ onSuccess}: UserFormProps) => {
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors, isSubmitting},
-    } = useForm<UserFormData>({
-        resolver: zodResolver(userSchema),  
-    })
-    const onSubmit = async (data: UserFormData) => {
-        try{
-            await createUser(data)
-            reset()
-            onSuccess()
-        }catch(err) {
-            console.error(err)
-        }
-    }
-    
-    return (
-        <form 
-        onSubmit={handleSubmit(onSubmit)}
-        className='bg-white p-6 rounded-xl shawdow-md space-y-4'
-        >
-            <div>
-                <input 
-                 {...register('name')}
-                 placeholder='Input Name'
-                 className='w-full border p-3 rounded-md'
-                />
-                {errors.name && (
-                    <p className='text-red-500 text-sm mt-1'> {errors.name.message}</p>
-                )}
-            </div>
+const UserForm = ({ onSuccess }: UserFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<UserFormData>({
+    resolver: zodResolver(userSchema),
+  })
 
-            <div>
+  const onSubmit = async (data: UserFormData) => {
+    try {
+      await createUser(data)
+      reset()
+      onSuccess()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-white p-6 rounded-xl shadow-md space-y-4"
+    >
+      <div>
+        <input
+          {...register('name')}
+          placeholder="Name"
+          className="w-full border p-3 rounded-md"
+        />
+        {errors.name && (
+          <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+        )}
+      </div>
+
+      <div>
         <input
           {...register('email')}
           placeholder="Email"
@@ -53,28 +54,26 @@ const UserForm = ({ onSuccess}: UserFormProps) => {
         )}
       </div>
 
+      <div>
+        <input
+          {...register('role')}
+          placeholder="Role"
+          className="w-full border p-3 rounded-md"
+        />
+        {errors.role && (
+          <p className="text-red-500 text-sm mt-1">{errors.role.message}</p>
+        )}
+      </div>
 
-            <div>
-                <input 
-                {...register('role')}
-                placeholder='Role'
-                className='w-full border p-3 rounded-md'
-                />
-                {errors.role && (
-                    <p className='text-red-500 text-sm mt-1'> {errors.role.message}</p>
-                )}
-            </div>
-           <button
-                type="submit"
-                disabled={isSubmitting}
-                className="w-full bg-black text-white py-3 rounded-md"
-            >
-                {isSubmitting ? 'Submitting...' : 'Submit'}
-            </button>
-        </form>
-    )
+      <button
+        type="submit"
+        disabled={isSubmitting}
+        className="w-full bg-black text-white py-3 rounded-md"
+      >
+        {isSubmitting ? 'Submitting...' : 'Submit'}
+      </button>
+    </form>
+  )
 }
-
-
 
 export default UserForm
