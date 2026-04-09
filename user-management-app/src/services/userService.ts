667947dd6ba7badcade6fe4e8e539380744bfb76
mrauthentik <umokeuchenna2020@gmail.com>
2026-04-09 11:18:35 +0100

    Created service layer to create and fetch users

---
+++ user-management-app/src/services/userService.ts
@@ -0,0 +1,22 @@
+import { supabase } from '../lib/supabase';
+import type { User } from '../types/user';
+
+export const createUser = async (payload: User) => {
+  const { data, error} = await supabase 
+    .from('users')
+    .insert([payload])
+   
+    if(error) throw new Error(error.message)
+
+        return data
+}
+
+export const fetchUsers = async ()=> {
+    const {data, error} = await supabase
+    .from("users")
+    .select("*")
+    .order("created_at", { ascending: false})
+
+    if(error) throw new Error(error.message)
+        return data
+}
